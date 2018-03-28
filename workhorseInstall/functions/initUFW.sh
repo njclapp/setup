31ec207d52e8453477ef981cb845139fc6273279
Nate Clapp <njclapp@pluto.dsu.edu>
2018-03-28 15:20:37 -0500

    Add dotfiles, finish scripts

--- workhorseInstall/functions/initUFW.sh
+++ workhorseInstall/functions/initUFW.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+ufw enable
+ufw deny from 192.168.1.1 to 224.0.0.1
+ufw allow from 192.168.1.0/24 to any port 80
+ufw allow from 192.168.1.0/24 to any port 137
+ufw allow from 192.168.1.0/24 to any port 138
+ufw allow from 192.168.1.0/24 to any port 139
+ufw allow from 192.168.1.0/24 to any port 445
+ufw allow from 192.168.1.0/24 to any port 1337
+ufw allow from 192.168.1.0/24 to any port 7777
+ufw allow from 192.168.1.0/24 to any port 8112
--- workhorseInstall/setup.sh
+++ workhorseInstall/setup.sh
@@ -4,8 +4,11 @@
 # VARIABLES
 HOSTNAME='workhorse'
 USER=''
-PACKAGES='apache2 deluge-web docker samba ssh git ddclient fortune cowsay sensors ufw'
-NETWORK_PACKAGES='nload htop nmap fping tcptrack'
+PACKAGES='apache2 deluge-web docker samba ssh git ddclient fortune cowsay lm-sensors tig'
+ADMINISTRATION_PACKAGES='nload htop nmap fping tcptrack'
+COLOR='\033[0;32m'
+PLAINTEXT='\033[0m'
+
 
 if ! whoami | grep -q root; then
     echo "Please restart this script as root"
@@ -14,22 +17,45 @@ else
   continue
 fi
 
-# Move Dotfiles
-
 # Initial Update
-apt-get update && sudo apt-get upgrade -y
+echo -e "${COLOR}[+] Updating System...1/8${PLAINTEXT}"
+apt-get update && apt-get upgrade -y
 apt-get dist-upgrade -y
 
 # Add/Clone Repos
+echo -e "${COLOR}[+] Cloning Repositories...2/8${PLAINTEXT}"
+git clone https://github.com/dylanaraps/neofetch.git
+cd neofetch
+make install
+cd ..
 
 # Install Packages
+echo -e "${COLOR}[+] Installing Packages...3/8${PLAINTEXT}"
 apt-get install $PACKAGES -y
-apt-get install $NETWORK_PACKAGES -y
+apt-get install $ADMINISTRATION_PACKAGES -y
 
 # Set up lm-sensors
+echo -e "${COLOR}[+] Initializing lm-sensors...4/8${PLAINTEXT}"
 sensors-detect --auto
 
+# Move Dotfiles
+echo -e "${COLOR}[+] Moving Dotfiles...5/8${PLAINTEXT}"
+mv dotfiles/.bashrc /home/$USER/
+mv dotfiles/.vimrc /home/$USER/
+mv dotfiles/interfaces /etc/network/
+mv dotfiles/smb.conf /etc/samba/
+mv dotfiles/sshd_config /etc/ssh/
+
+# Set up samba
+echo -e "${COLOR}[+] Adding User $USER to Samba...6/8${PLAINTEXT}"
+smbpasswd -a $USER
+
+# Initialize ufw rules
+echo -e "${COLOR}[+] Initializing UFW...7/8${PLAINTEXT}"
+. functions/initUFW.sh
+
 # Change Hostname
+echo -e "${COLOR}[+] Changing Hostname...8/8${PLAINTEXT}"
 hostn=$(cat /etc/hostname)
 sudo sed -i "s/$hostn/$HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$hostn/$HOSTNAME/g" /etc/hostname
