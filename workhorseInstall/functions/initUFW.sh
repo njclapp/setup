19a74a75b34fd3c0b1a82576fd1940c432f59834
Nate Clapp <njclapp@pluto.dsu.edu>
2018-03-28 15:58:22 -0500

    refactor ufw rule 1337

--- workhorseInstall/functions/initUFW.sh
+++ workhorseInstall/functions/initUFW.sh
@@ -7,6 +7,6 @@ ufw allow from 192.168.1.0/24 to any port 137
 ufw allow from 192.168.1.0/24 to any port 138
 ufw allow from 192.168.1.0/24 to any port 139
 ufw allow from 192.168.1.0/24 to any port 445
-ufw allow from 192.168.1.0/24 to any port 1337
-ufw allow from 192.168.1.0/24 to any port 7777
+ufw allow from any to any port 1337
+#ufw allow from 192.168.1.0/24 to any port 7777
 ufw allow from 192.168.1.0/24 to any port 8112
