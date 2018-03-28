20fbbd34d4f2e241cb62217b19b44ae0d20d4f0c
Nate Clapp <njclapp@pluto.dsu.edu>
2018-03-28 15:46:54 -0500

    Refactor Code

--- workhorseInstall/setup.sh
+++ workhorseInstall/setup.sh
@@ -4,7 +4,7 @@
 # VARIABLES
 HOSTNAME='workhorse'
 USER=''
-PACKAGES='apache2 deluge-web docker samba ssh git ddclient fortune cowsay lm-sensors tig'
+PACKAGES='apache2 deluge-web docker samba ssh git fortune cowsay lm-sensors tig make'
 ADMINISTRATION_PACKAGES='nload htop nmap fping tcptrack'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -12,35 +12,32 @@ PLAINTEXT='\033[0m'
 
 
 if ! whoami | grep -q root; then
-	echo -e "${RED}[!] Please run this script as root!${PLAINTEXT}"
+	echo "${RED}[!] Please run this script as root!${PLAINTEXT}"
     exit 1
 else
   continue
 fi
 
 # Initial Update
-echo -e "${GREEN}[+] Updating System...1/8${PLAINTEXT}"
+echo "${GREEN}[+] Updating System...1/9${PLAINTEXT}"
 apt-get update && apt-get upgrade -y
 apt-get dist-upgrade -y
 
 # Add/Clone Repos
-echo -e "${GREEN}[+] Cloning Repositories...2/8${PLAINTEXT}"
-git clone https://github.com/dylanaraps/neofetch.git
-cd neofetch
-make install
-cd ..
+echo "${GREEN}[+] Cloning Repositories...2/9${PLAINTEXT}"
+git clone https://github.com/dylanaraps/neofetch.git /home/$USER/neofetch
 
 # Install Packages
-echo -e "${GREEN}[+] Installing Packages...3/8${PLAINTEXT}"
+echo "${GREEN}[+] Installing Packages...3/9${PLAINTEXT}"
 apt-get install $PACKAGES -y
 apt-get install $ADMINISTRATION_PACKAGES -y
 
 # Set up lm-sensors
-echo -e "${GREEN}[+] Initializing lm-sensors...4/8${PLAINTEXT}"
+echo "${GREEN}[+] Initializing lm-sensors...4/9${PLAINTEXT}"
 sensors-detect --auto
 
 # Move Dotfiles
-echo -e "${GREEN}[+] Moving Dotfiles...5/8${PLAINTEXT}"
+echo "${GREEN}[+] Moving Dotfiles...5/9${PLAINTEXT}"
 mv dotfiles/.bashrc /home/$USER/
 mv dotfiles/.vimrc /home/$USER/
 mv dotfiles/interfaces /etc/network/
@@ -48,18 +45,25 @@ mv dotfiles/smb.conf /etc/samba/
 mv dotfiles/sshd_config /etc/ssh/
 
 # Set up samba
-echo -e "${GREEN}[+] Adding User $USER to Samba...6/8${PLAINTEXT}"
+echo "${GREEN}[+] Adding User $USER to Samba...6/9${PLAINTEXT}"
 smbpasswd -a $USER
 
 # Initialize ufw rules
-echo -e "${GREEN}[+] Initializing UFW...7/8${PLAINTEXT}"
+echo "${GREEN}[+] Initializing UFW...7/9${PLAINTEXT}"
 . functions/initUFW.sh
 
 # Change Hostname
-echo -e "${GREEN}[+] Changing Hostname...8/8${PLAINTEXT}"
+echo "${GREEN}[+] Changing Hostname...8/9${PLAINTEXT}"
 hostn=$(cat /etc/hostname)
 sudo sed -i "s/$hostn/$HOSTNAME/g" /etc/hosts
 sudo sed -i "s/$hostn/$HOSTNAME/g" /etc/hostname
+
+# Disable IPv6
+echo "${GREEN}[+] Disabling IPv6...9/9${PLAINTEXT}"
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -p
 
 # Script is finished. Wait and reboot
 echo "Post-install has completed. Rebooting in 10 seconds..."
