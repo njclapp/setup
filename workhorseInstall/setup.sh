5518cbf3e4d92c8276a2a6d1725d026218668da6
Nate Clapp <njclapp@pluto.dsu.edu>
2018-03-28 15:23:40 -0500

    Add root warning

--- workhorseInstall/setup.sh
+++ workhorseInstall/setup.sh
@@ -11,7 +11,7 @@ PLAINTEXT='\033[0m'
 
 
 if ! whoami | grep -q root; then
-    echo "Please restart this script as root"
+	echo -e "\033[031m[!] Please run this script as root!\033[0m"
     exit 1
 else
   continue
