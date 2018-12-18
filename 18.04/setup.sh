e18fbcbcae35d44730c6f0039d2e0aa92e0f2cbc
Nate Clapp <njclapp1@gmail.com>
2018-12-17 20:45:30 -0600

    add themes and DE customizations

--- 18.04/setup.sh
+++ 18.04/setup.sh
@@ -3,8 +3,9 @@
 
 # VARIABLES
 USER=''
-BASIC_PACKAGES='vim htop git lm-sensors atom unity-tweak-tool vlc steam chromium-browser putty sl tig uprecords-cgi qemu-kvm libvirt-bin bridge-utils virt-manager gnome-multi-writer minecraft-installer neofetch rename'
+BASIC_PACKAGES='vim htop git lm-sensors atom unity-tweak-tool vlc steam chromium-browser putty sl tig uprecords-cgi qemu-kvm libvirt-bin bridge-utils virt-manager gnome-multi-writer minecraft-installer neofetch rename dconf-editor'
 SYSADMIN_PACKAGES='nmap fping rdesktop tcptrack nload'
+THEMES='ultra-flat-icons '
 DISCORD_VERSION='0.0.5'
 
 # Check if user is root
@@ -25,6 +26,12 @@ apt-get install $BASIC_PACKAGES -y
 # Sysadmin related packages
 apt-get install $SYSADMIN_PACKAGES -y
 
+# Install themes
+apt-get install $THEMES
+
+git clone https://github.com/vinceliuice/vimix-gtk-themes.git /opt/vimix
+cd /opt/vimix && ./Install
+
 # Move dotfiles
 mv .bashrc /home/$USER/; chown $USER:$USER /home/$USER/.bashrc
 mv .vimrc /home/$USER/; chown $USER:$USER /home/$USER/.vimrc
@@ -39,3 +46,14 @@ wget -4 https://imgur.com/a/VN0a0Vi/zip && unzip zip && rm -rf zip
 # Get wallpapers
 cd /home/$USER/Pictures/wallpapers
 wget -4 https://imgur.com/a/29jWnHn/zip && unzip zip && rm -rf zip
+
+
+# Change DE settings
+# Make clicking active window icon minimize window
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+# Set keyboard shortcut for wallpaper script
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Run wallpaper script'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'notify-send "help"'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Launch1'
