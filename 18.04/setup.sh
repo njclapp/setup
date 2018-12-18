94c1c5d55472b42d1992bf62bfa3465cdbe3b927
Nate Clapp <njclapp1@gmail.com>
2018-12-17 22:28:11 -0600

    more gsettings additions

--- 18.04/setup.sh
+++ 18.04/setup.sh
@@ -5,7 +5,7 @@
 USER=''
 BASIC_PACKAGES='vim htop git lm-sensors atom unity-tweak-tool vlc steam chromium-browser putty sl tig uprecords-cgi qemu-kvm libvirt-bin bridge-utils virt-manager gnome-multi-writer minecraft-installer neofetch rename dconf-editor'
 SYSADMIN_PACKAGES='nmap fping rdesktop tcptrack nload'
-THEMES='ultra-flat-icons '
+THEMES='ultra-flat-icons arc-theme papirus-icon-theme'
 DISCORD_VERSION='0.0.5'
 
 # Check if user is root
@@ -15,6 +15,9 @@ if ! whoami | grep -q root; then
 else
   continue
 fi
+
+# Add theme repos before update
+sudo add-apt-repository ppa:papirus/papirus -y
 
 # Initial Update
 apt-get update && sudo apt-get upgrade -y
@@ -27,7 +30,7 @@ apt-get install $BASIC_PACKAGES -y
 apt-get install $SYSADMIN_PACKAGES -y
 
 # Install themes
-apt-get install $THEMES
+apt-get install $THEMES -y
 
 git clone https://github.com/vinceliuice/vimix-gtk-themes.git /opt/vimix
 cd /opt/vimix && ./Install
@@ -49,11 +52,32 @@ wget -4 https://imgur.com/a/29jWnHn/zip && unzip zip && rm -rf zip
 
 
 # Change DE settings
+
+# Set icon and window theme
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+gsettings set org.gnome.desktop.interface gtk-theme Vimix-dark-doder
+
 # Make clicking active window icon minimize window
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+# Place dash on bottom
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+# Icon size adjustment
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 36
 
 # Set keyboard shortcut for wallpaper script
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Run wallpaper script'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'notify-send "help"'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Launch1'
+
+# Disable accidental screenshot button presses
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot ''
+
+# Show date in top bar
+gsettings set org.gnome.desktop.interface clock-show-date true
+
+# Show battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
